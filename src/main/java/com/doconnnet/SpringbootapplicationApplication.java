dbe12a7790f9a8fab33f147d64bb989424be15e5
ChandraAkhilM <munaganurichandraakhil@gmail.com>
2022-07-16 02:18:09 +0530

    initial commit

---
+++ src/main/java/com/doconnnet/SpringbootapplicationApplication.java
@@ -0,0 +1,20 @@
+package com.doconnnet;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapplicationApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapplicationApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/com/doconnnet/SpringbootapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doconnnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
